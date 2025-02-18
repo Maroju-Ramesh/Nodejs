74871ffd81ab4fdd26b5004015a6d91216af8b63
maroju-rameshc <your.email@example.com>
2025-02-18 23:19:39 +0530

    updated

--- node1/index.js
+++ node1/index.js
@@ -1,6 +1,7 @@
 const http = require("http")
 const server =  http.createServer((req,res)=>{
     res.write("Every step of struggle brings a game of victory.....");
+    res.write("Every step of struggle brings a game of victory.....");
     res.end("")
 });
 server.listen("3101", ()=>{
