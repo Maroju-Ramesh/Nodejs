d4bd6a199c3bffe97e0c339cf6954b92a03f9b79
maroju-rameshc <your.email@example.com>
2025-02-18 23:22:28 +0530

    updated write

--- node1/index.js
+++ node1/index.js
@@ -1,9 +1,9 @@
 const http = require("http")
 const server =  http.createServer((req,res)=>{
     res.write("Every step of struggle brings a game of victory.....");
-    res.write("Every step of struggle brings a game of victory.....");
+    res.write("Just like a Node.js server, patiently handling requests and delivering success!");
     res.end("")
 });
 server.listen("3101", ()=>{
-    console.log("ust like a Node.js server, patiently handling requests and delivering success!")
+    console.log("Server is running")
 })
