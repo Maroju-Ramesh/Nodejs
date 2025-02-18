3443833c4abad3724113f540bbdf2d5f9a33de4c
maroju-rameshc <your.email@example.com>
2025-02-18 23:13:50 +0530

    updated

---
+++ node1/index.js
@@ -0,0 +1,8 @@
+const http = require("http")
+const server =  createServer((req,res)=>{
+    res.write("Every step of struggle brings a game of victory.....");
+    res.end("")
+});
+server.listen("3101", ()=>{
+    console.log("ust like a Node.js server, patiently handling requests and delivering success!")
+})
---
+++ node1/module1.js
@@ -0,0 +1,5 @@
+ console.log("iam module 1")
+
+ var z = "karthik";
+
+ module.exports={z:z}
